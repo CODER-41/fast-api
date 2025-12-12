73a5d03ef12ac1d32cbc690e1bb4d67c64fad553
RONNY <mboyaronny41@gmail.com>
2025-12-12 12:17:00 +0300

    fast api blog

--- main.py
+++ main.py
@@ -1,11 +1,57 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
+@app.get('/blog')
 
-    return  {'data': {'name': 'Code sorcerer'}}
+def index(limit=10, published: bool =True, sort: Optional[str] = None):
+
+    if published:
+     
+     return  {'data': f'{limit} blog from the db'}
+    
+    else:
+       return  {'data': f'{limit} blog from the db'}
+
+@app.get('/blog/unpublished')
+
+def unpublished():
+
+    return {'data': 'all unpublished blogs'}
 
 
+@app.get('/blog/{id}')
+
+def show(id):
+    #fetch blog with id = id
+     
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+
+def comments(id):
+
+    return {'data': {'1' , '2'}}
+
+#Using POST method
+
+class Blog(BaseModel):
+   
+   title: str
+
+   body:str
+   published: Optional[bool]
+
+   #pass
+
+
+
+@app.post('/blog')
+
+def create_blog(request: Blog):
+
+    return {'data': f"Blog is created wit title {request.title}"}
