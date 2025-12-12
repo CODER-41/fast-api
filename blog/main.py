084caad7e11cfd9740309ee922f67ca703157bc6
RONNY <mboyaronny41@gmail.com>
2025-12-12 14:55:13 +0300

    fast -api

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine,SessionLocal
 from sqlalchemy.orm import Session
@@ -18,7 +18,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
@@ -28,6 +28,49 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog{id}', status_code=status.HTTP_204_NO_CONTENT)
+
+def destroy(id,db: Session = Depends(get_db)):
+
+
+    blog=db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+       raise HTTPException(
+           status_code=status.HTTP_404_NOT_FOUND,
+           detail=f"Blog with id {id} not found")
+   
+   
+ 
+                    
+    blog.delete(synchronize_session=False)
+    
+    db.commit()
+
+    return 'done'
+
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+
+def update(id, request:schemas.Blog, db: Session = Depends(get_db)):
+   
+   blog=db.query(models.Blog).filter(models.Blog.id == id)
+   
+   if not blog.first():
+       raise HTTPException(
+           status_code=status.HTTP_404_NOT_FOUND,
+           detail=f"Blog with id {id} not found")
+   
+   blog.update(request.model_dump())
+
+   db.commit()
+    
+   return 'updated succesfully'
+
+
+
+
+
 
 @app.get('/blog')
 
@@ -36,3 +79,20 @@ def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
 
     return blogs
+
+
+@app.get('/blog/{id}', status_code=200)
+
+def show(id: int, response: Response, db: Session = Depends(get_db),):
+
+    blog=db.query(models.Blog).filter(models.Blog.id == id).first()
+
+    if not blog:
+
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the id {id} is not available")
+
+        # response.status_code = status.HTTP_404_NOT_FOUND
+
+        # return {'detail': f"Blog with the id {id} is not available"}
+
+    return blog
